ac132058315e7d7de63d8b34bb79bc043c28aada
Ruben Burgos Diaz <RubenBurgos@github.com>
2024-06-18 00:00:23 +0200

    nmap implementado al 40%

---
+++ TFG/source_code/tools/type_tools/implemented_tools/active_info_tools/nmap.py
@@ -0,0 +1,16 @@
+from nmap import PortScanner # type: ignore
+from source_code.tools.type_tools.active_info_tool import Active_Info_Tool
+from source_code.data.data_type import Data_Type
+
+class Nmap(Active_Info_Tool):
+    def __init__(self, scan_data_manager):
+        super().__init__("Nmap", [Data_Type.IP], scan_data_manager)
+
+    def scan(self):
+        nmap = PortScanner()
+        nmap.scan(self.scan_data_manager.input_data.data, arguments="--version-intensity 9 -A -p- -vvv")
+
+    def load_fetched_info(self):
+        pass
+
+# Nmap --version-intensity 9 -A -p- -vvv <ip X.x.x.z>
